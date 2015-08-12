daurnimator/lua-http

commit c2a7e5260bfcd28effcb5d201399158afc75b182

http/h1_connection: Remove :next_header function

http/h1_stream.lua
@@ -144,13 +144,14 @@ function stream_methods:get_headers(timeout)
 	end
 	-- Use while loop for lua 5.1 compatibility
 	while true do
-		local k, v, errno = self.connection:next_header(deadline and (deadline-monotime()))
+		local k, v = self.connection:read_header(deadline and (deadline-monotime()))
 		if k == nil then
-			if v == nil then
-				break
-			else
-				return nil, v, errno
+			-- if it was an error, it will be repeated
+			local ok, err, errno2 = self:read_headers_done(deadline and (deadline-monotime()))
+			if ok == nil then
+				return nil, err, errno2
 			end
+			break -- Success: End of headers.
 		end
 		k = k:lower() -- normalise to lower case
 		if k == "host" then
@@ -346,13 +347,14 @@ local function read_body_iter(headers)
 				-- read trailers
 				-- TODO: check against trailer header as whitelist?
 				while true do
-					local k, v, errno2 = self.connection:next_header()
+					local k, v = self.connection:read_header(deadline and (deadline-monotime()))
 					if k == nil then
-						if v == nil then
-							break
-						else
-							return nil, v, errno2
+						-- if it was an error, it will be repeated
+						local ok, err, errno2 = self:read_headers_done(deadline and (deadline-monotime()))
+						if ok == nil then
+							return nil, err, errno2
 						end
+						break -- Success: End of headers.
 					end
 					self.headers:append(k, v)
 				end

http/h1_connection.lua
@@ -193,20 +193,6 @@ function connection_methods:read_headers_done(timeout)
 	end
 end
 
-function connection_methods:next_header(timeout)
-	local deadline = timeout and (monotime()+timeout)
-	local key, val = self:read_header(timeout)
-	if key == nil then
-		-- if it was an error, it will be repeated
-		local ok, err, errno2 = self:read_headers_done(deadline and (deadline-monotime()))
-		if ok == nil then
-			return nil, err, errno2
-		end
-		return -- Success: End of headers.
-	end
-	return key, val
-end
-
 -- pass a negative length for *up to* that number of bytes
 function connection_methods:read_body_by_length(len, timeout)
 	assert(type(len) == "number")

spec/h1_connection_spec.lua
@@ -160,25 +160,6 @@ describe("low level http 1 connection operations", function()
 				assert.same(t[2], v)
 			end
 			assert(s:read_headers_done())
-
-			-- Test 'next_header' as well
-			assert(c:write_request_line("GET", "/", 1.1))
-			for _, t in ipairs(input) do
-				assert(c:write_header(t[1], t[2]))
-			end
-			assert(c:write_headers_done())
-
-			assert(s:read_request_line())
-			local i = 0
-			while true do
-				local k, v = s:next_header()
-				if k == nil then break end
-				i = i + 1
-				local t = input[i]
-				assert.same(t[1], k)
-				assert.same(t[2], v)
-			end
-			assert.same(#input, i)
 		end
 		test{}
 		test{

commit 90496829643de624c0f3fe74fc76c580a2fb57cd

http/h1_stream: Fix use of wrong object (introduced in c2a7e5260bfcd28effcb5d201399158afc75b182)

http/h1_stream.lua
@@ -147,7 +147,7 @@ function stream_methods:get_headers(timeout)
 		local k, v = self.connection:read_header(deadline and (deadline-monotime()))
 		if k == nil then
 			-- if it was an error, it will be repeated
-			local ok, err, errno2 = self:read_headers_done(deadline and (deadline-monotime()))
+			local ok, err, errno2 = self.connection:read_headers_done(deadline and (deadline-monotime()))
 			if ok == nil then
 				return nil, err, errno2
 			end
@@ -350,7 +350,7 @@ local function read_body_iter(headers)
 					local k, v = self.connection:read_header(deadline and (deadline-monotime()))
 					if k == nil then
 						-- if it was an error, it will be repeated
-						local ok, err, errno2 = self:read_headers_done(deadline and (deadline-monotime()))
+						local ok, err, errno2 = self.connection:read_headers_done(deadline and (deadline-monotime()))
 						if ok == nil then
 							return nil, err, errno2
 						end