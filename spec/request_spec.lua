d15328d2474a72699ee0c3e53ae7b44b5f55f418
daurnimator <quae@daurnimator.com>
2015-12-19 01:13:23 +1100

    spec/request_spec: Add tests for new_from_uri with username and password

--- spec/request_spec.lua
+++ spec/request_spec.lua
@@ -23,6 +23,19 @@ describe("http.request module", function()
 			assert.same("https", req.headers:get ":scheme")
 			assert.same(nil, req.body)
 		end
+		do -- with userinfo section
+			local base64 = require "base64"
+			local req = request.new_from_uri("https://user:password@example.com/")
+			assert.same("example.com", req.host)
+			assert.same(443, req.port)
+			assert.truthy(req.tls)
+			assert.same("example.com", req.headers:get ":authority")
+			assert.same("GET", req.headers:get ":method")
+			assert.same("/", req.headers:get ":path")
+			assert.same("https", req.headers:get ":scheme")
+			assert.same("user:password", base64.decode(req.headers:get "authorization":match "^basic%s+(.*)"))
+			assert.same(nil, req.body)
+		end
 	end)
 	it("fails on invalid URIs", function()
 		assert.has.errors(function() request.new_from_uri("not a URI") end)
