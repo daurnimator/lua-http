77ea4b74ca1f669f675c22bf2a62c8a4b58f6511
daurnimator <quae@daurnimator.com>
2015-12-20 02:21:48 +1100

    http/server: Refactor to have a 'server' object. **breaks API**

--- http/server.lua
+++ http/server.lua
@@ -42,11 +42,10 @@ end
 -- Wrap a bare cqueues socket in a http connection of a suitable version
 -- Starts TLS if necessary
 -- this function *should never throw*
-local function wrap_socket(socket, tbl, ctx, timeout)
-	local deadline = timeout and (monotime()+timeout)
+local function wrap_socket(self, socket, deadline)
 	socket:setmode("b", "b")
 	socket:onerror(onerror)
-	local use_tls = tbl.tls
+	local use_tls = self.tls
 	if use_tls == nil then
 		local err, errno
 		use_tls, err, errno = is_tls_client_hello(socket, deadline and (deadline-monotime()))
@@ -56,7 +55,7 @@ local function wrap_socket(socket, tbl, ctx, timeout)
 	end
 	local is_h2 -- tri-state
 	if use_tls then
-		local ok, err, errno = socket:starttls(ctx, deadline and (deadline-monotime()))
+		local ok, err, errno = socket:starttls(self.ctx, deadline and (deadline-monotime()))
 		if not ok then
 			return nil, err, errno
 		end
@@ -148,21 +147,46 @@ local function new_ctx(host)
 	return ctx
 end
 
--- Starts listening on the given socket
-local function listen(tbl, on_stream, cq)
-	local running = assert(cqueues.running(), "must be running inside of a cqueue")
-	cq = cq or running
+local server_methods = {
+	max_concurrent = math.huge;
+	client_timeout = 10;
+}
+local server_mt = {
+	__name = "http.server";
+	__index = server_methods;
+}
+
+--[[ Starts listening on the given socket
+
+Takes a table of options:
+  - `.host`: address to bind to (required)
+  - `.port`: port to bind to (optional if tls isn't `nil`, in which case defaults to 80 for `.tls == false` or 443 if `.tls == true`)
+  - `.v6only`: allow ipv6 only (no ipv4-mapped-ipv6)
+  - `.reuseaddr`: turn on SO_REUSEADDR flag?
+  - `.reuseport`: turn on SO_REUSEPORT flag?
+  - `.tls`: `nil`: allow both tls and non-tls connections
+  -         `true`: allows tls connections only
+  -         `false`: allows non-tls connections only
+  - `.ctx`: an `openssl.ssl.context` object to use for tls connections
+  - `       `nil`: a self-signed context will be generated
+  - `.max_concurrent`: Maximum number of connections to allow live at a time (default: infinity)
+  - `.client_timeout`: Timeout (in seconds) to wait for client to send first bytes and/or complete TLS handshake (default: 10)
+]]
+local function listen(tbl)
 	local tls = tbl.tls
 	local host = assert(tbl.host, "need host")
-	local port = tbl.port or tls and "443" or "80"
-	local max_concurrent = tbl.max_concurrent or math.huge
-	assert(max_concurrent > 0)
-	-- timeout waiting for client to send first bytes and/or complete TLS handshake
-	local wrap_timeout = tbl.client_timeout or 10
-	local ctx
-	if type(tls) == "userdata" then
-		ctx = tls
-	elseif tls ~= false then
+	local port = tbl.port
+	if port == nil then
+		if tls == true then
+			port = "443"
+		elseif tls == false then
+			port = "80"
+		else
+			error("need port")
+		end
+	end
+	local ctx = tbl.ctx
+	if ctx == nil and tls ~= false then
 		ctx = new_ctx(host)
 	end
 	local s = assert(cs.listen{
@@ -172,24 +196,59 @@ local function listen(tbl, on_stream, cq)
 		reuseaddr = tbl.reuseaddr;
 		reuseport = tbl.reuseport;
 	})
-	s:listen() -- Actually wait for and *do* the binding
 	-- Return errors rather than throwing
 	s:onerror(function(s, op, why, lvl) -- luacheck: ignore 431 212
 		return why
 	end)
 
-	local n = 0
-	local done = cc.new()
+	return setmetatable({
+		socket = s;
+		tls = tls;
+		ctx = ctx;
+		max_concurrent = tbl.max_concurrent;
+		n_connections = 0;
+		connection_done = cc.new(); -- signalled when connection has been closed
+		client_timeout = tbl.client_timeout;
+	}, server_mt)
+end
+
+-- Actually wait for and *do* the binding
+-- Don't *need* to call this, as if not it will be done lazily
+function server_methods:listen(timeout)
+	return self.socket:listen(timeout)
+end
+
+function server_methods:localname()
+	self.socket:localname()
+end
+
+function server_methods:shutdown()
+	self.socket:shutdown()
+end
+
+function server_methods:close()
+	self:shutdown()
+	cqueues.poll()
+	cqueues.poll()
+	self.socket:close()
+end
+
+-- accepts a new client and returns it as an http connection object
+function server_methods:run(on_stream, cq)
+	cq = cq or cqueues.running()
 	while true do
-		if n >= max_concurrent then
-			done:wait()
+		if self.n_connections >= self.max_concurrent then
+			self.connection_done:wait()
 		end
 		-- Yield this thread until a client arrives
-		local socket, accept_err = s:accept{nodelay = true;}
+		local socket, accept_err = self.socket:accept{nodelay = true;}
 		if socket == nil then
-			if accept_err == ce.EMFILE then
+			if accept_err == ce.EINVAL then
+				-- has been shutdown
+				break
+			elseif accept_err == ce.EMFILE then
 				-- Wait for another request to finish
-				if not done:wait(0.1) then
+				if not self.connection_done:wait(0.1) then
 					-- If we're stuck waiting for more than 100ms, run a garbage collection sweep
 					-- This can prevent a hang
 					collectgarbage()
@@ -198,10 +257,10 @@ local function listen(tbl, on_stream, cq)
 				error(ce.strerror(accept_err))
 			end
 		else
-			n = n + 1
+			self.n_connections = self.n_connections + 1
 			cq:wrap(function()
 				local ok, err
-				local conn, is_h2, errno = wrap_socket(socket, tbl, ctx, wrap_timeout)
+				local conn, is_h2, errno = wrap_socket(self, socket)
 				if not conn then
 					err = is_h2
 					socket:close()
@@ -212,8 +271,8 @@ local function listen(tbl, on_stream, cq)
 					ok, err = handle_client(conn, on_stream)
 					conn:close()
 				end
-				n = n - 1
-				done:signal(1)
+				self.n_connections = self.n_connections - 1
+				self.connection_done:signal(1)
 				if not ok
 					and err ~= ce.EPIPE -- client closed connection
 					and err ~= ce.ETIMEDOUT -- an operation timed out
@@ -223,6 +282,7 @@ local function listen(tbl, on_stream, cq)
 			end)
 		end
 	end
+	return true
 end
 
 return {
---
+++ spec/server_spec.lua
@@ -0,0 +1,44 @@
+local TEST_TIMEOUT = 2
+describe("server module", function()
+	local server = require "http.server"
+	local cqueues = require "cqueues"
+	local cs = require "cqueues.socket"
+	local function assert_loop(cq, timeout)
+		local ok, err, _, thd = cq:loop(timeout)
+		if not ok then
+			if thd then
+				err = debug.traceback(thd, err)
+			end
+			error(err, 2)
+		end
+	end
+	it("works", function()
+		local cq = cqueues.new()
+		local s = server.listen {
+				host = "127.0.0.1";
+				port = 8000;
+		}
+		s:listen()
+		local on_stream = spy.new(function(stream)
+			stream:get_headers()
+			stream:shutdown()
+			s:shutdown()
+		end)
+		cq:wrap(function()
+			s:run(on_stream)
+		end)
+		cq:wrap(function()
+			local c = assert(cs.connect{
+				host = "127.0.0.1";
+				port = 8000;
+			})
+			assert(c:connect())
+			c:setmode("b", "nb")
+			assert(c:write("GET / HTTP/1.0\r\n\r\n", "n"))
+			c:read()
+		end)
+		assert_loop(cq, TEST_TIMEOUT)
+		assert.truthy(cq:empty())
+		assert.spy(on_stream).was.called()
+	end)
+end)
