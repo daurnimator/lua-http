dc631a14e3920ed72b97cdb6b6deffed2b2e0e44
daurnimator <quae@daurnimator.com>
2016-03-27 23:43:35 +1100

    spec/compat_socket_spec: Add test that uses full luasocket http.request form

--- spec/compat_socket_spec.lua
+++ spec/compat_socket_spec.lua
@@ -90,4 +90,58 @@ describe("http.compat.socket module", function()
 		assert_loop(cq, TEST_TIMEOUT)
 		assert.truthy(cq:empty())
 	end)
+	it("works against builtin server with complex request", function()
+		local cq = cqueues.new()
+		local s = server.listen {
+			host = "localhost";
+			port = 0;
+		}
+		assert(s:listen())
+		local _, host, port = s:localname()
+		cq:wrap(function()
+			s:run(function (stream)
+				s:pause()
+				local request_headers = assert(stream:get_headers())
+				assert.same("http", request_headers:get ":scheme")
+				assert.same("PUT", request_headers:get ":method")
+				assert.same("/path?query", request_headers:get ":path")
+				assert.same("otherhost.com:8080", request_headers:get ":authority")
+				assert.same("fun", request_headers:get "myheader")
+				assert.same("normalised", request_headers:get "camelcase")
+				assert(stream:write_continue())
+				local body = assert(stream:get_body_as_string())
+				assert.same("a body", body)
+				local headers = new_headers()
+				headers:append(":status", "404")
+				headers:append("connection", "close")
+				assert(stream:write_headers(headers, false))
+				assert(stream:write_chunk("hello world", true))
+			end)
+			s:close()
+		end)
+		cq:wrap(function()
+			local r, e = assert(http.request {
+				url = "http://example.com/path?query";
+				host = host;
+				port = port;
+				method = "PUT";
+				headers = {
+					host = "otherhost.com:8080";
+					myheader = "fun";
+					CamelCase = "normalised";
+				};
+				source = coroutine.wrap(function()
+					coroutine.yield("a body")
+				end);
+				sink = coroutine.wrap(function(b)
+					assert.same("hello world", b)
+					assert.same(nil, coroutine.yield(true))
+				end);
+			})
+			assert.same(1, r)
+			assert.same(404, e)
+		end)
+		assert_loop(cq, TEST_TIMEOUT)
+		assert.truthy(cq:empty())
+	end)
 end)
