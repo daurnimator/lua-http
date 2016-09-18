ac6f8d8e67459e882a727ef88b266b6806696d06
daurnimator <quae@daurnimator.com>
2016-09-18 20:21:50 +1000

    http/util: Add function 'read_proxy_vars' for reading proxies from environment variables
    Related to #23

--- http/util.lua
+++ http/util.lua
@@ -171,6 +171,70 @@ local function to_authority(host, port, scheme)
 	return authority
 end
 
+-- Proxy from e.g. environmental variables.
+local proxies_methods = {}
+local proxies_mt = {
+	__name = "http.util.proxies";
+	__index = proxies_methods;
+}
+
+-- Finds the correct proxy for a given scheme/host
+function proxies_methods:choose(scheme, host)
+	if self.no_proxy == "*" then
+		return nil
+	elseif self.no_proxy then
+		-- cache no_proxy_set by overwriting self.no_proxy
+		if type(self.no_proxy) == "string" then
+			local no_proxy_set = {}
+			-- wget allows domains in no_proxy list to be prefixed by "."
+			-- e.g. no_proxy=.mit.edu
+			for host_suffix in self.no_proxy:gmatch("%.?([^,]+)") do
+				no_proxy_set[host_suffix] = true
+			end
+			self.no_proxy_set = no_proxy_set
+		end
+		-- From curl docs:
+		-- matched as either a domain which contains the hostname, or the
+		-- hostname itself. For example local.com would match local.com,
+		-- local.com:80, and www.local.com, but not www.notlocal.com.
+		for pos in host:gmatch("%f[^%z%.]()") do
+			local host_suffix = host:sub(pos, -1)
+			if self.no_proxy_set[host_suffix] then
+				return nil
+			end
+		end
+	end
+	if scheme == "http" or scheme == "ws" then
+		if self.http_proxy then
+			return self.http_proxy
+		end
+	elseif scheme == "https" or scheme == "wss" then
+		if self.https_proxy then
+			return self.https_proxy
+		end
+	end
+	return self.all_proxy
+end
+
+-- prefers lower case over upper case; except for http_proxy where no upper case
+local function read_proxy_vars(getenv)
+	if getenv == nil then
+		getenv = os.getenv
+	end
+	local r = setmetatable({
+		http_proxy = nil;
+		https_proxy = getenv "https_proxy" or getenv "HTTPS_PROXY";
+		all_proxy = getenv "all_proxy" or getenv "ALL_PROXY";
+		no_proxy = getenv "no_proxy" or getenv "NO_PROXY";
+	}, proxies_mt)
+	if getenv "GATEWAY_INTERFACE" then -- Mitigate httpoxy. see https://httpoxy.org/
+		r.http_proxy = getenv "CGI_HTTP_PROXY"
+	else
+		r.http_proxy = getenv "http_proxy"
+	end
+	return r
+end
+
 -- HTTP prefered date format
 -- See RFC 7231 section 7.1.1.1
 local function imf_date(time)
@@ -199,6 +263,7 @@ return {
 	scheme_to_port = scheme_to_port;
 	split_authority = split_authority;
 	to_authority = to_authority;
+	read_proxy_vars = read_proxy_vars;
 	imf_date = imf_date;
 	maybe_quote = maybe_quote;
 }
