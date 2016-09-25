b85b5a37aa8e1398e4c1fa1d17ddd916752bbf55
daurnimator <quae@daurnimator.com>
2016-09-25 17:27:43 +1000

    spec/proxies_spec: Restructure tests

--- spec/proxies_spec.lua
+++ spec/proxies_spec.lua
@@ -6,23 +6,41 @@ describe("http.proxies module", function()
 			http_proxy = "http://http.proxy";
 			https_proxy = "http://https.proxy";
 			all_proxy = "http://all.proxy";
-			no_proxy = "*";
+			no_proxy = nil;
 		})[k] end)
 		assert.same({
 			http_proxy = "http://http.proxy";
 			https_proxy = "http://https.proxy";
 			all_proxy = "http://all.proxy";
-			no_proxy = "*";
+			no_proxy = nil;
 		}, proxies)
+		assert.same("http://http.proxy", proxies:choose("http", "myhost"))
+		assert.same("http://https.proxy", proxies:choose("https", "myhost"))
+		assert.same("http://all.proxy", proxies:choose("other", "myhost"))
+	end)
+	it("isn't vulnerable to httpoxy", function()
+		assert.same({}, http_proxies.new():update(function(k) return ({
+			GATEWAY_INTERFACE = "CGI/1.1";
+			http_proxy = "vulnerable to httpoxy";
+		})[k] end))
+	end)
+	it("works with no_proxy set to *", function()
+		local proxies = http_proxies.new():update(function(k) return ({
+			http_proxy = "http://http.proxy";
+			https_proxy = "http://https.proxy";
+			all_proxy = "http://all.proxy";
+			no_proxy = "*";
+		})[k] end)
 		-- Should return nil due to no_proxy being *
 		assert.same(nil, proxies:choose("http", "myhost"))
 		assert.same(nil, proxies:choose("https", "myhost"))
 		assert.same(nil, proxies:choose("other", "myhost"))
-		proxies.no_proxy = nil
-		assert.same("http://http.proxy", proxies:choose("http", "myhost"))
-		assert.same("http://https.proxy", proxies:choose("https", "myhost"))
-		assert.same("http://all.proxy", proxies:choose("other", "myhost"))
-		proxies.no_proxy = "foo,bar.com,.extra.dot.com"
+	end)
+	it("works with a no_proxy set", function()
+		local proxies = http_proxies.new():update(function(k) return ({
+			http_proxy = "http://http.proxy";
+			no_proxy = "foo,bar.com,.extra.dot.com";
+		})[k] end)
 		assert.same("http://http.proxy", proxies:choose("http", "myhost"))
 		assert.same(nil, proxies:choose("http", "foo"))
 		assert.same(nil, proxies:choose("http", "bar.com"))
@@ -33,11 +51,5 @@ describe("http.proxies module", function()
 		assert.same(nil, proxies:choose("http", "more.extra.dot.com"))
 		assert.same(nil, proxies:choose("http", "extra.dot.com"))
 		assert.same("http://http.proxy", proxies:choose("http", "dot.com"))
-	end)
-	it("isn't vulnerable to httpoxy", function()
-		assert.same({}, http_proxies.new():update(function(k) return ({
-			GATEWAY_INTERFACE = "CGI/1.1";
-			http_proxy = "vulnerable to httpoxy";
-		})[k] end))
 	end)
 end)
