8d84a38bf6dc0e407ea144068a0802e7f3fd1448
daurnimator <quae@daurnimator.com>
2016-10-04 23:20:53 +1100

    examples/server_hello: log errors to stderr

--- examples/server_hello.lua
+++ examples/server_hello.lua
@@ -42,7 +42,15 @@ local myserver = http_server.listen {
 	host = "localhost";
 	port = port;
 	onstream = reply;
+	onerror = function(myserver, context, op, err, errno) -- luacheck: ignore 212
+		local msg = op .. " on " .. tostring(context) .. " failed"
+		if err then
+			msg = msg .. ": " .. tostring(err)
+		end
+		assert(io.stderr:write(msg, "\n"))
+	end;
 }
+
 -- Manually call :listen() so that we are bound before calling :localname()
 assert(myserver:listen())
 do
