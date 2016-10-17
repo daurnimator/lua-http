060a8020fe3edd4f25802c820f5c22795a29f1ef
daurnimator <quae@daurnimator.com>
2016-10-17 12:28:49 -0700

    examples/server_sent_events: Add HTML page so that user can preview in browser

--- examples/server_sent_events.lua
+++ examples/server_sent_events.lua
@@ -21,18 +21,45 @@ local myserver = http_server.listen {
 
 		-- Build response headers
 		local res_headers = http_headers.new()
-		res_headers:append(":status", "200")
-		res_headers:append("content-type", "text/event-stream")
-		res_headers:append("access-control-allow-origin", "*")
-		-- Send headers to client; end the stream immediately if this was a HEAD request
-		assert(stream:write_headers(res_headers, req_method == "HEAD"))
-		if req_method ~= "HEAD" then
-			-- Start a loop that sends the current time to the client each second
-			while true do
-				local msg = string.format("data: The time is now %s.\n\n", os.date())
-				assert(stream:write_chunk(msg, false))
-				cqueues.sleep(1) -- yield the current thread for a second.
+		if req_headers:get ":path" == "/" then
+			res_headers:append(":status", "200")
+			res_headers:append("content-type", "text/html")
+			-- Send headers to client; end the stream immediately if this was a HEAD request
+			assert(stream:write_headers(res_headers, req_method == "HEAD"))
+			if req_method ~= "HEAD" then
+				assert(stream:write_chunk([[
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EventSource demo</title>
+	<script type="text/javascript">
+		var events = new EventSource("/event-stream");
+		events.onmessage = function(e) {
+			document.body.innerHTML = e.data;
+		}
+	</script>
+</head>
+<body>
+</body>
+</html>
+]], true))
 			end
+		elseif req_headers:get ":path" == "/event-stream" then
+			res_headers:append(":status", "200")
+			res_headers:append("content-type", "text/event-stream")
+			-- Send headers to client; end the stream immediately if this was a HEAD request
+			assert(stream:write_headers(res_headers, req_method == "HEAD"))
+			if req_method ~= "HEAD" then
+				-- Start a loop that sends the current time to the client each second
+				while true do
+					local msg = string.format("data: The time is now %s.\n\n", os.date())
+					assert(stream:write_chunk(msg, false))
+					cqueues.sleep(1) -- yield the current thread for a second.
+				end
+			end
+		else
+			res_headers:append(":status", "404")
+			assert(stream:write_headers(res_headers, true))
 		end
 	end;
 	onerror = function(myserver, context, op, err, errno) -- luacheck: ignore 212
@@ -49,7 +76,7 @@ local myserver = http_server.listen {
 assert(myserver:listen())
 do
 	local bound_port = select(3, myserver:localname())
-	assert(io.stderr:write(string.format("Now listening on port %d\n", bound_port)))
+	assert(io.stderr:write(string.format("Now listening on port %d\nOpen http://localhost:%d/ in your browser\n", bound_port, bound_port)))
 end
 -- Start the main server loop
 assert(myserver:loop())
