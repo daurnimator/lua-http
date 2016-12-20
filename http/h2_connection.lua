daurnimator/lua-http

commit 4a3ad3fc8418e6943c0b18e3c320189d151a508e

http/h2_connection: Don't create new table in merge_settings

http/h2_connection.lua
@@ -34,15 +34,13 @@ local default_settings = {
 	[0x6] = math.huge;  -- MAX_HEADER_LIST_SIZE
 }
 
-local function merge_settings(new, old)
-	return {
-		[0x1] = new[0x1] or old[0x1];
-		[0x2] = new[0x2] or old[0x2];
-		[0x3] = new[0x3] or old[0x3];
-		[0x4] = new[0x4] or old[0x4];
-		[0x5] = new[0x5] or old[0x5];
-		[0x6] = new[0x6] or old[0x6];
-	}
+local function merge_settings(tbl, new)
+	for i=0x1, 0x6 do
+		local v = new[i]
+		if v ~= nil then
+			tbl[i] = v
+		end
+	end
 end
 
 local connection_methods = {}
@@ -470,7 +468,7 @@ function connection_methods:write_goaway_frame(last_stream_id, err_code, debug_m
 end
 
 function connection_methods:set_peer_settings(peer_settings)
-	self.peer_settings = merge_settings(peer_settings, self.peer_settings)
+	merge_settings(self.peer_settings, peer_settings)
 	self.peer_settings_cond:signal()
 end
 
@@ -480,7 +478,7 @@ function connection_methods:ack_settings()
 	local acked_settings = self.send_settings[n]
 	if acked_settings then
 		self.send_settings[n] = nil
-		self.acked_settings = merge_settings(acked_settings, self.acked_settings)
+		merge_settings(self.acked_settings, acked_settings)
 	end
 	self.send_settings_ack_cond:signal()
 end

commit f892f95ea1ea316cb6dee535d86e59962d2093e3

http/h2_connection: Fix one connection's settings modifying the defaults
Bug introduced in 4a3ad3fc8418e6943c0b18e3c320189d151a508e

http/h2_connection.lua
@@ -141,9 +141,9 @@ local function new_connection(socket, conn_type, settings)
 		recv_goaway = cc.new();
 		new_streams = new_fifo();
 		new_streams_cond = cc.new();
-		peer_settings = default_settings;
+		peer_settings = {};
 		peer_settings_cond = cc.new(); -- signaled when the peer has changed their settings
-		acked_settings = default_settings;
+		acked_settings = {};
 		send_settings = {n = 0};
 		send_settings_ack_cond = cc.new(); -- for when server ACKs our settings
 		send_settings_acked = 0;
@@ -154,6 +154,8 @@ local function new_connection(socket, conn_type, settings)
 		pongs = {}; -- pending pings we've sent. keyed by opaque 8 byte payload
 	}, connection_mt)
 	self:new_stream(0)
+	merge_settings(self.peer_settings, default_settings)
+	merge_settings(self.acked_settings, default_settings)
 	self.encoding_context = hpack.new(default_settings[known_settings.HEADER_TABLE_SIZE])
 	self.decoding_context = hpack.new(default_settings[known_settings.HEADER_TABLE_SIZE])
 