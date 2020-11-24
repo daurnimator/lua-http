6c3c69ce7c8bbcd68ba14c37954fd1c93b350ef0
John Regan <john@jrjrtech.com>
2020-11-24 08:54:52 -0500

    bit: check for loadstring and use it if it exists

--- http/bit.lua
+++ http/bit.lua
@@ -7,24 +7,17 @@ The bit operations are only done
 This means we can ignore the differences between bit libraries.
 ]]
 
--- Lua 5.1's load function doesn't support reading strings, only
--- functions.
-local function string_loader(str)
-	local sent = false
-	return function()
-		if sent then return nil end
-		sent = false
-		return str
-	end
-end
+-- Lua 5.1 uses loadstring for load with a string, Lua 5.2+ just
+-- uses load.
+local load = loadstring or load
 
 -- Lua 5.3+ has built-in bit operators, wrap them in a function.
 local info = debug.getinfo(1, "Sl")
-local has_bitwise, bitwise = pcall(load(string_loader(("\n"):rep(info.currentline+1)..[[return {
+local has_bitwise, bitwise = pcall(load(("\n"):rep(info.currentline+1)..[[return {
 	band = function(a, b) return a & b end;
 	bor = function(a, b) return a | b end;
 	bxor = function(a, b) return a ~ b end;
-}]]), info.source))
+}]], info.source))
 if has_bitwise then return bitwise end
 
 -- The "bit" library that comes with luajit
